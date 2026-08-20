ce3cda96f8c5f5a769a7fbcded29014baff5bc8b
balajibhuvi <balajibhuvi37@gmail.com>
2026-08-20 14:17:00 +0530

    updated

---
+++ JAVA_SCRIPT/day4/asstes/index.js
@@ -0,0 +1,51 @@
+let age = 25;
+
+if (age >= 18) {
+    console.log("Eligible");
+} else {
+    console.log("Not Eligible");
+}
+
+
+let number=15;
+
+if (number %2==0) {
+    console.log("this is even");
+    
+}else{
+    console.log("this is odd");
+    
+}
+
+let mark=85
+
+if (mark >=90&& mark<=100) {
+    console.log("A+");
+    
+}else if (mark >=75&& mark<=89) {
+    console.log("A");
+    
+}else if (mark >=50&& mark <=74) {
+    console.log("B");
+    
+}else if (mark >=35&&mark<=45) {
+     console.log("C");
+     
+}else {
+    console.log("fail");
+    
+}
+
+
+let username="admin"
+let password="1234"
+
+if (username === "admin" && password === "1234") {
+    console.log("Login Success");
+    
+}else{
+    console.log("invalid login");
+    
+}
+
+
