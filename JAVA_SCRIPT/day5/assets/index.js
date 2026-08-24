6280f4148e75a14d12fe570552299bde8e6b4901
balajibhuvi <balajibhuvi37@gmail.com>
2026-08-24 13:54:08 +0530

    updated task

---
+++ JAVA_SCRIPT/day5/assets/index.js
@@ -0,0 +1,71 @@
+for (let i = 1; i <= 10; i++) {
+    console.log(i);
+}
+
+
+for (let i = 10; i >= 1; i--) {
+    console.log(i);
+}
+
+for (let i = 1; i <= 20; i++) {
+    console.log(i);
+}
+
+
+
+for (let i = 1; i <= 20; i +=2) {
+    console.log(i);
+}
+
+
+for (let i = 1; i <= 20; i +=2) {
+    console.log(i);
+}
+
+
+
+let sum = 0;
+
+for (let i = 1; i <= 10; i++) {
+    sum = sum + i;
+}
+
+console.log(sum);
+
+for (let i = 10; i >= 5; i--) {
+    console.log(i);
+}
+ 
+for ( let a = 1; a <=10; a++){
+     console.log(a);
+     
+}
+    
+for (let a=1; a<=20; a++){
+
+    if(a%2==0)
+    {
+        console.log(a);
+        
+    }
+        
+}
+    for( let a=1; a <=20; a++){
+        if(a%2==1)
+            console.log(a);
+            
+    }
+
+let number=5;
+for(let i=1;i<=10;i++)
+{
+    console.log("5 X",i,"=",i*number);
+    
+}
+let sum1=0;
+for(let a=1; a<=10;a++){
+
+    sum1+=a;
+   
+}
+ console.log(sum1);
