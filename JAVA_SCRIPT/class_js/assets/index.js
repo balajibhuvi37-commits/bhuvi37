f196cff6b5b78750058fe6a6dfb8660b0fd3f2e7
balajibhuvi <balajibhuvi37@gmail.com>
2026-08-25 22:47:16 +0530

    updated task

---
+++ JAVA_SCRIPT/class_js/assets/index.js
@@ -0,0 +1,7 @@
+let a=""
+
+for(let a=1; a<=10; a--){
+
+}
+
+console.log(a);
---
+++ JAVA_SCRIPT/day6/assets/index.js
@@ -0,0 +1,15 @@
+let result = "";
+
+for (let i = 1; i <= 20; i++) {
+    result += i + " ";
+}
+
+console.log(result);
+
+
+// let answer="";
+
+// for(i = 20; i <= 40; i++){
+//     answer += i + " " ;
+// }
+// console.log(answer );
